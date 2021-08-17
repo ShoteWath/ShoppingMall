50e282b2aae7e414da6db90db58a8b52f8022860
ShoteWath <thanomsa@gmail.com>
2021-08-17 10:32:11 +0700

    edit product 17 August

--- lib/bodys/show_product_seller.dart
+++ lib/bodys/show_product_seller.dart
@@ -5,6 +5,7 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:shoppingmall/models/product_model.dart';
+import 'package:shoppingmall/states/edit_product.dart';
 import 'package:shoppingmall/utility/my_constant.dart';
 import 'package:shoppingmall/widgets/show_image.dart';
 import 'package:shoppingmall/widgets/show_progress.dart';
@@ -152,7 +153,15 @@ class _ShowProductSellerState extends State<ShowProductSeller> {
                     mainAxisAlignment: MainAxisAlignment.spaceAround,
                     children: [
                       IconButton(
-                          onPressed: () {},
+                          onPressed: () {
+                            print('## You Click Edit');
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => EditProduct(
+                                      productModel: productModels[index]),
+                                ));
+                          },
                           icon: Icon(
                             Icons.edit_outlined,
                             size: 36,
